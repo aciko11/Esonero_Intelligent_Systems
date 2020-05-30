87b7810b7417b19c4736802ec08af253db883ec6
aciko11 <federico.baldoni11@gmail.com>
2020-05-30 10:16:54 +0200

    worked on stopwords
    Done the remove part, need to add the text with the stopwords removed into a new array of lists

--- Scraper.py
+++ Scraper.py
@@ -2,8 +2,14 @@
 #E' necessario il download del driver corrispondente per il browser a https://chromedriver.chromium.org/ (nel caso di utilizzo del browser Chrome
 from selenium import webdriver
 import json
-number_of_pages = 300
-driver = webdriver.Chrome("./chromedriver.exe")
+import nltk
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+import os.path
+
+
+number_of_pages = 1
+driver = webdriver.Chrome()
 driver.get("https://ricerca.repubblica.it/ricerca/repubblica?query=+mascherine&fromdate=2020-03-15&todate=2020-04-15&sortby=ddate&author=&mode=all")
 #driver.get("https://ricerca.repubblica.it/ricerca/repubblica?query=+mascherine&fromdate=2020-03-15&todate=2020-04-15&sortby=ddate&author=&mode=all")
 articles_list=[]
@@ -30,3 +36,47 @@ for page in range(number_of_pages):
     except:
         driver.close()
 driver.close()
+
+
+
+############# Cleaning the file from stopwords #############
+#region cleaning_the_file_from_stopwords
+
+
+# List that will contain the stop words
+stop_words = []
+
+#region check_for_my_corrected_stop_words_file 
+if not os.path.exists("./stopwords.txt"):
+    nltk.download("stopwords")
+    nltk.download('punkt')
+
+    temp_stop_words = stopwords.words("italian")
+    print(stop_words)
+    for word in temp_stop_words:
+        stop_words.append(word)
+        stop_words.append(word.title())
+    with open("stopwords.txt", "w") as file_out:
+        for item in stop_words:
+            file_out.write("%s\n" % item)
+else:
+    with open("stopwords.txt", "r") as stop_words_file:
+        for line in stop_words_file:
+            stop_words.append(line[:-1])
+
+#endregion check_for_my_corrected_stop_words_file
+
+# Open the json file with all the data from the search
+with open("nomefile.json") as json_file:
+    data = json.load(json_file)
+
+temp_words = []
+for t in data:
+    print("Titolo non filtrato: ")
+    print(t["title"])
+    word_tokens = word_tokenize(t["title"])
+    temp_words.append([word for word in word_tokens if not word in stop_words])
+    print("Titolo filtrato: ")
+    print(temp_words[-1])
+
+#endregion cleaning_the_file_from_stopwords
